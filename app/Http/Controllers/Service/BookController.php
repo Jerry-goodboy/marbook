4a25dc0f6c4f37a4335eba84cf43003d312937f6
Moearly <982221982@qq.com>
2016-03-28 21:57:57 +0800

    错误修改

--- app/Http/Controllers/Service/BookController.php
+++ app/Http/Controllers/Service/BookController.php
@@ -9,11 +9,13 @@ use App\Models\M3Result;
 
 class BookController extends Controller
 {
+    //根据id返回详情
     public function getCategoryByParentId($parent_id)
     {
         $categorys = Category::where('parent_id', $parent_id)->get();//查询父类别
         $result = new M3Result();
         $result->message = "返回成功";
+
         $result->status = '0';
         $result->categorys = $categorys;
         return $result->toJson();
